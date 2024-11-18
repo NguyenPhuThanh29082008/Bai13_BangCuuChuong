ef20b63eac857dce582771a340814348546ca95a
NguyenPhuThanh29082008 <nguyenphuthanhpch298@gmail.com>
2024-11-18 10:34:50 +0700

    Add project files.

---
+++ Bai13_BangCuuChuong.cpp
@@ -0,0 +1,20 @@
+// Bai13_BangCuuChuong.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "BANG CUU CHUONG" << endl;
+	for (int i = 1; i <= 10; i++)
+	{
+		for (int so = 2; so < 10; so++)
+		{
+			cout << so << " x " << i << " = " << so * i << "\t";
+		}
+		cout << endl;
+	}
+	return 0;
+
+}
+
